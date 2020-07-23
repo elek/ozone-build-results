4227ed7756081c51645491056f89fb72aed9d3cd
Doroszlai, Attila <adoroszlai@apache.org>
2020-07-23 13:19:39 +0200

    only search build results

--- find-first.sh
+++ find-first.sh
@@ -9,12 +9,16 @@ if which rg >& /dev/null; then
   cmd='rg'
 fi
 
-${cmd} "$@" *
+${cmd} "$@" 2???
 
 #First build with the problem
-BUILD=$(${cmd} -l "$@" * | awk -F '/' '{print $4}' | sort -n | head -n 1)
+BUILD=$(${cmd} -l "$@" 2??? | awk -F '/' '{print $4}' | sort -n | head -n 1)
 
-FAILED_BUILD_DIR=$(find * -name $BUILD -type d)
+if [[ -z "${BUILD}" ]]; then
+  exit 1
+fi
+
+FAILED_BUILD_DIR=$(find * -name "$BUILD" -type d)
 FIRST_FAILURE_COMMIT=$(jq -r '.jobs[0].head_sha' $FAILED_BUILD_DIR/job.json)
 echo "First failed commit: $FIRST_FAILURE_COMMIT"
 OZONE_DIR=${OZONE_DIR:-$PWD/../ozone}
